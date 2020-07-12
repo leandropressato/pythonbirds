fb804cc668c5109d02646b4cfc36e67296cd18b2
leandropressato <leandrocp14@hotmail.com>
2020-07-12 19:56:44 -0300

    Unittest

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -17,8 +17,11 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos{cls.olhos}'
 
+class Homem(Pessoa):
+    pass
+
 if __name__ == '__main__':
-    leandro = Pessoa(nome='Leandro')
+    leandro = Homem(nome='Leandro')
     luciano = Pessoa(leandro, nome='Luciano')
     for filho in luciano.filhos:
         print(filho.nome)
@@ -31,4 +34,7 @@ if __name__ == '__main__':
     print(id(Pessoa.olhos), id(luciano.olhos), id(leandro.olhos))
     print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+
+    pessoa=Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
 
